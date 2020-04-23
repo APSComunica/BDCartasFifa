30867bbdae1b7bbad6f789d51f48cde6f40a9f30
Usuario <Usuario@Usuario-VAIO>
2020-04-23 11:38:54 +0200

    segundo

--- src/main/java/es/andresp/prueba1sb/App.java
+++ src/main/java/es/andresp/prueba1sb/App.java
@@ -7,6 +7,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * JavaFX App
@@ -15,21 +20,39 @@ public class App extends Application {
 
     private static Scene scene;
     private EntityManagerFactory emf;
-    private EntityManager em;
-
+    private static EntityManager em;
+    static JuadoresViewController juadoresViewController;
+    
     @Override
     public void start(Stage stage) throws IOException {
+        emf = Persistence.createEntityManagerFactory("es.andresp_CartasFifa_jar_1.0-SNAPSHOTPU");
+        em = emf.createEntityManager();
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
+        
     }
 
+    @Override
+    public void stop() throws Exception {
+        em.close(); 
+        emf.close(); 
+        try { 
+        DriverManager.getConnection("jdbc:derby:BDCartasFifa;shutdown=true"); 
+        } catch (SQLException ex) { 
+        }
+    }
+
+    
+    
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        juadoresViewController = (JuadoresViewController) fxmlLoader.getController();
+        juadoresViewController.setEntityManager(em);
         return fxmlLoader.load();
     }
 
